98f40b9f60762a012220c4b1708e1873266bbe46
Gligor Kotushevski <gligorkot@gmail.com>
2018-03-09 13:55:46 +1300

    adding TestUtils.swift

---
+++ TestUtils.swift
@@ -0,0 +1,19 @@
+//
+//  TestUtils.swift
+//
+//  Created by Gligor Kotushevski on 9/03/18.
+//  Copyright © 2018 Gligor Kotushevski. All rights reserved.
+//
+
+import Foundation
+
+let defaultTimeout: TimeInterval = 0.2
+
+// used for stubbing json objects in tests
+internal func stubbedObject(_ filename: String) -> Data! {
+    @objc class TestClass: NSObject { }
+
+    let bundle = Bundle(for: TestClass.self)
+    let path = bundle.path(forResource: filename, ofType: "json")
+    return (try? Data(contentsOf: URL(fileURLWithPath: path!)))
+}
